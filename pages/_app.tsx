077917dc94eb6dc11ce4d19459be47863da17bf0
ra2dev <raramanouski@gmail.com>
2022-08-15 15:22:49 +0200

    Added tailwind/prettier.git

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import '../styles/variables.css'
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,111 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.navigation}>
+        <div>swik</div>
+        <div>Login</div>
+      </nav>
+    </header>
+  );
+};
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Setup swik <a href="https://nextjs.org">SWIK starter</a>
-        </h1>
+    <>
+      <Header />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Setup swik <a href="https://nextjs.org">SWIK starter</a>
+          </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <p className={styles.description}>
+            Get started by editing{" "}
+            <code className={styles.code}>pages/index.js</code>
+          </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <div className={styles.grid}>
+            <a href="https://nextjs.org/docs" className={styles.card}>
+              <h3>Documentation &rarr;</h3>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
+
+            <svg
+              fill="currentColor"
+              role="img"
+              aria-hidden="true"
+              className="gamut-j03qo6-Svg e3tf18d0"
+            >
+              <title>Dot Loose</title>
+              <pattern
+                id="DotLoose-pattern-3"
+                x="0"
+                y="0"
+                width="16"
+                height="16"
+                patternUnits="userSpaceOnUse"
+              >
+                <rect width="0.5" height="0.5" fill="currentColor"></rect>
+                <rect y="1" width="0.5" height="0.5" fill="currentColor"></rect>
+                <rect
+                  y="0.5"
+                  width="0.5"
+                  height="0.5"
+                  fill="currentColor"
+                ></rect>
+                <rect x="1" width="0.5" height="0.5" fill="currentColor"></rect>
+                <rect
+                  x="1"
+                  y="1"
+                  width="0.5"
+                  height="0.5"
+                  fill="currentColor"
+                ></rect>
+                <rect
+                  x="1"
+                  y="0.5"
+                  width="0.5"
+                  height="0.5"
+                  fill="currentColor"
+                ></rect>
+                <rect
+                  x="0.5"
+                  width="0.5"
+                  height="0.5"
+                  fill="currentColor"
+                ></rect>
+                <rect
+                  x="0.5"
+                  y="1"
+                  width="0.5"
+                  height="0.5"
+                  fill="currentColor"
+                ></rect>
+                <rect
+                  x="0.5"
+                  y="0.5"
+                  width="0.5"
+                  height="0.5"
+                  fill="currentColor"
+                ></rect>
+              </pattern>
+              <rect
+                width="100%"
+                height="100%"
+                fill="url(#DotLoose-pattern-3)"
+              ></rect>
+            </svg>
+          </div>
+        </main>
+
+        <footer className={styles.footer}>
+          <a href="https://ra2.dev" target="_blank" rel="noopener noreferrer">
+            Powered by&nbsp;<b>RA2</b>
           </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+        </footer>
+      </div>
+    </>
+  );
 }
