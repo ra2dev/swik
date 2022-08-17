ceacb4dfaa33c0b78799c7fade6209cf0347f29d
ra2dev <raramanouski@gmail.com>
2022-08-17 22:41:47 +0200

    WIP on layout/samples

---
+++ components/Logo.tsx
@@ -0,0 +1,15 @@
+export const Logo = () => {
+    return (
+        <div className='flex items-center p-1 pr-2 cursor-pointer hover:border-lime-500 border-2 rounded-md border-transparent'>
+            <svg width='41' height='41' viewBox='0 0 41 41' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                <path
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    d='M20.6836 40.1379C31.7293 40.1379 40.6836 31.1836 40.6836 20.1379C40.6836 9.09225 31.7293 0.137939 20.6836 0.137939C9.63788 0.137939 0.683594 9.09225 0.683594 20.1379C0.683594 31.1836 9.63788 40.1379 20.6836 40.1379ZM26.9229 9.45478C27.2266 8.37599 26.1797 7.73807 25.2236 8.41924L11.8767 17.9275C10.8398 18.6662 11.0029 20.1379 12.1217 20.1379H15.6363V20.1107H22.4861L16.9048 22.08L14.4443 30.8211C14.1406 31.8999 15.1874 32.5378 16.1436 31.8566L29.4905 22.3484C30.5274 21.6097 30.3642 20.1379 29.2455 20.1379H23.9157L26.9229 9.45478Z'
+                    fill='#a3e636'
+                />
+            </svg>
+            <div className='ml-2 font-semibold text-gray-800 decoration-lime-400 underline decoration-4 underline-offset-1'>SWIK&nbsp;</div>
+        </div>
+    )
+}
---
+++ components/layout.tsx
@@ -0,0 +1,108 @@
+import {Logo} from "./Logo"
+import cn from "classnames"
+import {useState} from "react"
+
+const Separator = () => (
+    <svg
+        data-testid='geist-icon'
+        fill='none'
+        height='32'
+        shapeRendering='geometricPrecision'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='1'
+        viewBox='0 0 24 24'
+        width='32'
+        style={{color: "#00000073"}}
+    >
+        <path d='M16.88 3.549L7.12 20.451'></path>
+    </svg>
+)
+
+const Tabs = ({items}: {items: {title: string}[]}) => {
+    const [active, setActive] = useState(items[0]?.title)
+    return (
+        <div className='flex'>
+            {items?.map((e, i) => {
+                const isActive = e.title === active
+                return (
+                    <div
+                        className={cn("mr-2 p-2 pr-3 border-b-2 cursor-pointer", {
+                            "border-b-gray-500": isActive
+                        })}
+                        key={i}
+                        onClick={() => setActive(e.title)}
+                    >
+                        {e.title}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export const Navigation = () => {
+    return (
+        <header
+            className='w-full flex flex-col flex-row items-center  border-gray-300 border-b-1 border-solid border'
+            style={{}}
+        >
+            <nav className='ml-auto container mr-auto max-w-5xl flex flex-row items-center text-gray-600 text-sm h-14'>
+                <div>
+                    <Logo />
+                </div>
+                <div className='ml-auto flex items-center'>
+                    <div className='cursor-pointer hover:underline hover:decoration-lime-400  hover:decoration-4'>
+                        Support
+                    </div>
+                    <div>
+                        <button
+                            className='ml-4 text-gray-600 text-sm p-1 pl-2 pr-2 rounded-md border-2 border-gray-400 border active:border-gray-400 hover:border-gray-400 focus:border-gray-400'
+                            onClick={() => alert("register")}
+                        >
+                            Register
+                        </button>
+                    </div>
+                </div>
+            </nav>
+            <div className='flex ml-auto container mr-auto max-w-5xl text-sm mt-2'>
+                <Tabs items={[{"title": "Utilities"}, {"title": "Short Link"}]} />
+            </div>
+        </header>
+    )
+}
+export const Footer = () => {
+    return (
+        <footer className='w-full border-t-1 border-gray-300 border-t-1 border-solid border-t'>
+            <div className='container ml-auto mr-auto max-w-5xl text-center p-2 pb-5 flex justify-center mt-4 mb-4'>
+                <a href='https://ra2.dev' target='_blank' rel='noopener noreferrer' className='mr-auto ml-auto flex'>
+                    <span>
+                        Powered by&nbsp;<b className='decoration-amber-500 underline decoration-4'>RA2</b>
+                    </span>
+                    <span
+                        className='text-sm'
+                        style={{
+                            writingMode: "vertical-rl",
+                            textOrientation: "mixed"
+                        }}
+                    >
+                        .dev
+                    </span>
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export const Layout = (props: any) => {
+    return (
+        <>
+            <Navigation />
+            <main className='container ml-auto mr-auto h-20 pt-10 pr-10 max-w-5xl mb-20 min-h-screen'>
+                {props.children}
+            </main>
+            <Footer />
+        </>
+    )
+}
